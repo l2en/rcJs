a72725b5a4c18553ed13ebc4e24022e9d085cbf7
l2en <33387383+l2en@users.noreply.github.com>
2019-10-11 16:13:11 +0800

    Update rc.js

--- rc.js
+++ rc.js
@@ -33,26 +33,6 @@
         tags: ["React", "知识点", "要点", "笔记"],
         path: articleBaseUrl+'React要点.md',
         isOverview: true
-      },
-      {
-        id: '201910111500',
-        title: "That's Testing",
-        des: "Testing for sth...",
-        lag: ['None'],
-        see: 230,
-        tags: ["None"],
-        path: articleBaseUrl+'React要点.md',
-        isOverview: true
-      },
-      {
-        id: '201910111501',
-        title: "That's Testing uhhhhhhhh",
-        des: "Testing for sth...",
-        lag: ['None'],
-        see: 230,
-        tags: ["None"],
-        path: articleBaseUrl+'React要点.md',
-        isOverview: true
       }
     ]
   }
